b43ed9e68f3414765669420feeac04479d61d452
SergeySadykovN <snsadykov@ya.ru>
2024-04-22 14:28:16 +0300

    add calculate_and_display_average_price(data)

--- data_download.py
+++ data_download.py
@@ -10,3 +10,11 @@ def fetch_stock_data(ticker, period='1mo'):
 def add_moving_average(data, window_size=5):
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
+
+
+def calculate_and_display_average_price(data):
+    '''вычисляет и выводит среднюю цену закрытия акций
+    за заданный период.
+    '''
+    average_period = data['Close'].mean()
+    return print(f'Average closing price of shares for a given period: {average_period}')
